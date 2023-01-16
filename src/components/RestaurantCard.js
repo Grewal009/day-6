36bd632711aac11ac42fd097b236723eedb6405b
aneel.grewal <aneel.grewal@hotmail.com>
2023-01-16 12:14:51 +0100

    Destructuring the props on the fly in RestaurantCard

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,13 +1,14 @@
 import {IMG_CDN} from '../constants'
 import { RESTAURANT_API } from '../constants';
 
-    const RestaurantCard = (props) => {
+    const RestaurantCard = ({restaurant}) => {
+        const {cloudinaryImageId, name, cuisines, avgRating} = restaurant.data;
         return(
             <div className="card">
-                <img src={IMG_CDN + props.restaurant?.data?.cloudinaryImageId}/>
-                <h2>{props.restaurant?.data?.name}</h2>
-                <h3>{props.restaurant?.data?.cuisines?.join(", ")}</h3>
-                <h3>Avg Rating {props.restaurant?.data?.avgRating}</h3>
+                <img src={IMG_CDN + cloudinaryImageId}/>
+                <h2>{name}</h2>
+                <h3>{cuisines?.join(", ")}</h3>
+                <h3>Avg Rating {avgRating}</h3>
             </div>
         );
     }
