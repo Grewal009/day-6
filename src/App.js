8a67f84ae72d59c7c33a8cbcc7f064dedfded741
aneel.grewal <aneel.grewal@hotmail.com>
2023-01-19 11:00:23 +0100

    updated assignment 7

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ReactDOM  from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-import { createBrowserRouter, Route, RouterProvider, Outlet} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
